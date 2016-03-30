a41046d89bfde5adf4c6dbdadaf34aca972ed016
Mitlond <comp122012016@mail.ru>
2016-03-30 09:56:42 +0300

    c++->PHP

---
+++ s__translation_in_php.php
@@ -0,0 +1,26 @@
+<meta charset="utf-8">
+<?php
+$arrayMod=arrayMod(200);   //массив от 1 до 200
+for ($count=0;$count<=200;$count++){
+$arrayMod[$count] = $count + 1; // заполним массив числами от 1 до 200
+}
+$sum=0;
+$divider=0;
+$ad=0;
+for ($i = 0;$i<=200;$i++)
+{
+if (($arrayMod[$i]%arrayMod[j])==0)
+{
+if ((arrayMod[ad] = ArrayMod[j];
+}
+};
+if ($divider==5)
+{
+$sum+=$arrayMod[$i];
+echo "<br />";
+echo$arrayMod[$i];
+echo",";
+}
+echo$sum;
+echo"<br />";
+?>
